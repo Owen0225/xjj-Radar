193005258f592d77a48638fd3e8e7d05daf8ef7e
Object0225 <xiaowei@chinabaiker.com>
2018-04-27 14:21:31 +0800

    更新 update.sh

--- update.sh
+++ update.sh
@@ -28,7 +28,7 @@ cd libpcap-1.8.1
 make
 make install
 
-git clone https://github.com/794959818/PUBG-Radar-Onekey.git
+git clone https://gitee.com/Object0225/xjj-Radar.git
 cd PUBG-Radar-Onekey/
 npm i
 npm i -g pino
